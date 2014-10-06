812c8e2e7d6ab3a69c80f2ea2873f9bf4baf5a39
Eric Chalmers <eric@granify.com>
2014-10-06 16:19:17 -0600

    Added VariableExtractor Class

---
+++ GeneticFuzzyModel/VariableExtractor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelling;
+using FuzzyInference;
+
+namespace GeneticFuzzyModelling
+{
+    class VariableExtractor
+    {
+        public Variable[] extractVariables(Modelling.DataSet data)
+        {
+            Variable[] vars = new Variable[data.FeatureNames.Length];
+            for (int i=0; i<vars.Length; i++)
+            {
+                vars[i] = new Variable(i, data.FeatureNames[i], data.featureMax(i), data.featureMin(i));
+            }
+
+            return vars;
+        }
+
+        public Variable extractOutputVariable(Modelling.DataSet data)
+        {
+            return new Variable(0, "output", data.YMax(), data.YMin());
+        }
+    }
+}
